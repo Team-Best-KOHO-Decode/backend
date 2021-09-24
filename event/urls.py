7f15f08e69a9b04c02332bf0b27076f5bb180278
Amanda Nguyen <amanda6@ualberta.ca>
2021-09-24 14:34:17 -0600

    changed int to str type

--- event/urls.py
+++ event/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns= [
         path('create', views.create_event),
-        path('<int:event_id>', views.get_event_by_id),
+        path('<str:event_id>', views.get_event_by_id),
         path('updateBudget', views.update_event_budget),
         path('activities', views.select_activities),
 ]
