4721735172e26af8ce89b7595ccd2a08bfd8a425
Amanda Nguyen <amanda6@ualberta.ca>
2021-09-24 14:47:07 -0600

    moved get id function last

--- event/urls.py
+++ event/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns= [
         path('create', views.create_event),
-        path('<str:event_id>', views.get_event_by_id),
         path('updateBudget', views.update_event_budget),
         path('activities', views.select_activities),
+        path('<str:event_id>', views.get_event_by_id),
 ]
 
