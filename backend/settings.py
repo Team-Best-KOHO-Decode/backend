69dbeaec67d77ecb6c1885e0238c27a22535da7a
YoussefZayed <Youssef.ah.Zayed@gmail.com>
2021-09-23 20:21:14 -0400

    group model

--- backend/settings.py
+++ backend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['bohobackend.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'group.apps.GroupConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- group/models.py
+++ group/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Group(models.Model):
+    name = models.CharField(max_length=200)
+    join_code = uuid.uuid4().hex[:6].upper()
+    events = [""]
+    
