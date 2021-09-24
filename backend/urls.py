0994dc18872be20e1b871744a82b1f40a8d49b2a
AlexWong12345 <33202724+AlexWong12345@users.noreply.github.com>
2021-09-23 20:46:08 -0400

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('group/', include('group.urls')),
+    path('api/group/', include('group.urls')),
     path('admin/', admin.site.urls),
 ]
