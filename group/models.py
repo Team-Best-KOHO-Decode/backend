b77ffa48d1e711bc6acab8131c1a21bf75616570
YoussefZayed <Youssef.ah.Zayed@gmail.com>
2021-09-23 23:11:18 -0400

    updated models

--- group/models.py
+++ group/models.py
@@ -5,8 +5,7 @@ import uuid
 class Group(models.Model):
     name = models.CharField(max_length=200)
     join_code = models.CharField(max_length=5, blank=True, unique=True, default=uuid.uuid4().hex[:5].upper()) 
-    # # TODO add refrence to events
-    # events = models.ForeignKey(Event)
-    # # TODO add refrence to users
-    # users = [""]
+    # JSON string of events and activities with ratings
+    events = models.CharField(max_length=9000)
+    
     
